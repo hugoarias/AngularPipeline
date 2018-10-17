d946b570ae1477652a11a36fb2882610276fd79c
Hugo Arias <hugo.arias@niceincontact.com>
2018-10-17 17:46:59 -0400

    Updating test dependencies

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -9,6 +9,8 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
+      require('karma-junit-reporter'),
+      require('karma-phantomjs-launcher'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
